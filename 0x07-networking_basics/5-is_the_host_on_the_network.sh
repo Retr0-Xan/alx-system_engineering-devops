fbf22da5d5eff752081acea45432b07f8a179276
__Mark__ <113648128+Retr0-Xan@users.noreply.github.com>
2023-03-08 01:14:45 +0000

    Update 5-is_the_host_on_the_network.sh

--- 0x07-networking_basics/5-is_the_host_on_the_network.sh
+++ 0x07-networking_basics/5-is_the_host_on_the_network.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #Displays Usage: 5-is_the_host_on_the_network {IP_ADDRESS} if no argument passed
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]
+then
     echo "Usage:5-is_the_host_on_the_network {IP_ADDRESS}"
     exit 1
 fi
