a346fa2972ffcb4d68b765160d5fd3411d13ea98
__Mark__ <113648128+Retr0-Xan@users.noreply.github.com>
2023-03-08 01:16:14 +0000

    Update 5-is_the_host_on_the_network.sh

--- 0x07-networking_basics/5-is_the_host_on_the_network.sh
+++ 0x07-networking_basics/5-is_the_host_on_the_network.sh
@@ -2,12 +2,8 @@
 #Displays Usage: 5-is_the_host_on_the_network {IP_ADDRESS} if no argument passed
 if [ $# -eq 0 ]
 then
-    echo "Usage:5-is_the_host_on_the_network {IP_ADDRESS}"
-    exit 1
+  echo "Usage: 5-is_the_host_on_the_network {IP_ADDRESS}"
+else
+  ping -c 5 "$1"
 fi
-
-ip_address=$1
-
-# Ping the IP address 5 times
-ping -c 5 "$ip_address"
 
